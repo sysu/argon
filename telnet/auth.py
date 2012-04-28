9d0510604136856ecd1d4abd028413f44a4b17d8
Norman Mo <LTaoist6@gmail.com>
2012-04-29 01:46:23 +0800

    重写了菜单、欢迎、认证部分

--- telnet/auth.py
+++ telnet/auth.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
-
+import sys
+sys.path.append('../')
+    
 from chaofeng import Frame,EndInterrupt,Timeout
 from chaofeng.g import mark,static
-from chaofeng.ui import TextInput,Password
+from chaofeng.ui import TextInput,Password,Animation,ColMenu
+from lib import check_user_exist,check_user_password
+from datetime import datetime
+import config
 import chaofeng.ascii as ac
 
 '''
@@ -10,15 +15,6 @@ import chaofeng.ascii as ac
 
 可能的跳转是到相应的功能区，结束。
 '''
-
-def check_username(username):
-    return True
-
-def check_user(username,passwd):
-    return True
-
-def login(username,passwd):
-    return True
 
 @mark('welcome')
 class WelcomeFrame(Frame):
@@ -32,10 +28,10 @@ class WelcomeFrame(Frame):
 
         self.write(self.background % { "online" : 0 })
 
-        p_username   = self.hint[0]
-        p_username_w = self.hint[1]
-        p_password   = self.hint[2]
-        p_password_w = self.hint[3]
+        p_username   =  self.hint[0]
+        p_username_w =  '\r\n'+self.hint[1]+'\r\n'
+        p_password   =  '\r\n'+self.hint[2]
+        p_password_w =  '\r\n'+self.hint[3]+'\r\n'
 
         input_name = TextInput(self)
         input_passwd = Password(self)
@@ -44,20 +40,72 @@ class WelcomeFrame(Frame):
             while True:
                 self.write(p_username)
                 username = input_name.read()
-                self.write('\r\n')
                 if username == 'new' :
                     self.goto(mark['register'])
                 elif username == 'guest' :
                     session['username'] = 'guest'
-                    self.goto(mark['menu'],'main')
-                elif not check_username(username) :
+                    self.goto(mark['menu'])
+                elif not check_user_exist(username) :
                     self.write(p_username_w)
                     continue
                 self.write(p_password)
                 password = input_passwd.read()
-                self.write('\r\n')
-                if check_user(username,password):
-                    login(username,password)
-                    self.goto(mark['menu'],'main')
+                if check_user_password(username,password):
+                    self.session['username'] = username
+                    self.goto(mark['menu'])
                 self.write(p_password_w)
+
+@mark('menu')
+class MenuFrame(Frame):
+
+    info_format = "%s区 [%s]"
+    
+    background = static['menu'].safe_substitute(
+        pos="%(pos)8s",
+        pos_info="%(pos_info)10s",
+        board="%(board)s",
+        content="%(content)s",
+        time="%(time)24s",
+        online="%(online)4d",
+        online_friend="%(online_friend)4d",
+        username="%(username)10s")
+
+    def initialize(self,name="main"):
+        self.anim = Animation(self,static['active'],start=3)
+        self.anim.run_bg()
+
+        pos = self.session.get('pos')
+        if not pos :
+            pos = ''
+            pos_info = ''
+        else:
+            pos_info = self.info_format % (self.session['pos_num'],
+                                           self.session['boardname'])
+            
+        self.write(self.background % {
+                "pos": pos,
+                "pos_info": pos_info,
+                "board":self.anim.fetch()[0],
+                "content":static['menu_'+name],
+                "time":datetime.now().ctime(),
+                "online":0,
+                "online_friend":0,
+                "username":self.session['username']})
+
+        next_f,kwargs = ColMenu(self,config.menu[name]).read()
+        self.goto(mark[next_f],**kwargs)
+
+@mark('unf')
+class UnfinishFrame(Frame):
+
+    def initialize(self):
+        self.write("This part isn't finish.")
+        self.close()
+
+@mark('bye')
+class ByeFrame(Frame):
+
+    def initialize(self):
+        self.write("Bye!.\r\n")
+        self.close()
 
--- telnet/config.py
+++ telnet/config.py
@@ -4,7 +4,17 @@ from chaofeng import ascii
 root = 'welcome'
 
 menu = {
-    "main":zip( zip(range(12,23),(5,)*11),range(1,12),'edfrmtiscpg'),
-    "cat":zip( zip(range(11,21),(7,)*10),range(1,11),'0123456789') +
-    zip(zip(range(11,14),(41,)*3),'ANE','ANE'),
+    "main":(
+        ( ("menu",{"name":"setion"}),'e',(12,5)),
+        ( ("unf",{}),'d' ),
+        ( ("unf",{}),'f' ),
+        ( ("unf",{}),'r' ),
+        ( ("unf",{}),'m' ),
+        ( ("unf",{}),'t' ),
+        ( ("unf",{}),'i' ),
+        ( ("unf",{}),'s' ),
+        ( ("unf",{}),'c' ),
+        ( ("unf",{}),'p' ),
+        ( ("bye",{}),'g' )),
+    "cat":[],
     }
