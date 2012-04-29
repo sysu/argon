3c410f8399294c834bd93455f06cadf59c30f715
Norman Mo <LTaoist6@gmail.com>
2012-04-29 08:27:28 +0800

    完成了welcome和menu(normal.py)

--- telnet/config.py
+++ telnet/config.py
@@ -5,7 +5,7 @@ root = 'welcome'
 
 menu = {
     "main":(
-        ( ("menu",{"name":"setion"}),'e',(12,5)),
+        ( ("menu",{"name":"section"}),'e',(12,5)),
         ( ("unf",{}),'d' ),
         ( ("unf",{}),'f' ),
         ( ("unf",{}),'r' ),
@@ -16,5 +16,20 @@ menu = {
         ( ("unf",{}),'c' ),
         ( ("unf",{}),'p' ),
         ( ("bye",{}),'g' )),
-    "cat":[],
-    }
+    "section":(
+        ( ( ("boardlist",{"sid":0}),'0',(11,6)),
+          ( ("boardlist",{"sid":1}),'1'),
+          ( ("boardlist",{"sid":2}),'2'),
+          ( ("boardlist",{"sid":3}),'3'),
+          ( ("boardlist",{"sid":4}),'4'),
+          ( ("boardlist",{"sid":5}),'5'),
+          ( ("boardlist",{"sid":6}),'6'),
+          ( ("boardlist",{"sid":7}),'7'),
+          ( ("boardlist",{"sid":8}),'8'),
+          ( ("boardlist",{"sid":9}),'9'),
+          ( ("boardlist",{}),            'a',(11,41)),
+          ( ("boardlist",{"new":True}),  'n'),
+          ( ("menu",     {"name":"main"}),'e'),
+          )
+    )
+}
