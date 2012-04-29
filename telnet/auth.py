29b609c0f6eb8f2d28054c4f0d1781a84a5c14c5
Norman Mo <LTaoist6@gmail.com>
2012-04-29 08:02:13 +0800

    增加frame的文档

--- telnet/auth.py
+++ telnet/auth.py
@@ -18,6 +18,20 @@ import chaofeng.ascii as ac
 
 @mark('welcome')
 class WelcomeFrame(Frame):
+    '''
+    实现欢迎界面。
+
+    调用lib/check_user_exit和lib\check_user_password进行验证。
+
+    成功登陆后跳转到 name=main 的menu frame。输入new转入注册，输入guest以
+    guest为username登陆。
+
+    没有最大尝试要求，但超过120s还没有登陆将会自动关闭连接。
+
+    本页面也是全局入口，在config.root设定。
+    
+    @para none
+    '''
 
     background = static['welcome'].safe_substitute(online="%(online)4s")
 
@@ -57,7 +71,31 @@ class WelcomeFrame(Frame):
 
 @mark('menu')
 class MenuFrame(Frame):
+    '''
+    浏览全部菜单的类。菜单的名字通过name在调用initialize时传入。
+    
+    将会以session['pos']和session['board_num']和session['boardname']作为
+    当前所在的位置，讨论区的编号，讨论区的名字输出。
 
+    电子公告版取active.ani作为内容。
+
+    online和online_friend用于输出在线人数（未实现）。
+
+    利用config中的menu[name]来设置菜单(ColMenu类),eg :
+       ( (fram_mark,goto_kwargs),shortcuts, [ (pos_l,pos_r) ] )
+       ...
+    其中frame_mark表示将会跳转到mark[frame_mark]，goto_kwargs是跳转时的
+    参数，是一个字典，shortcuts是快捷键。如果需要，可以加入 (pos_l,pos_r)
+    表示这个菜单项的位置，如果不加，默认在上一项的下面。
+
+    keypoint:
+         static['active'] 为电子公告板的内容
+         config.menu[name] 为该菜单的相关设定
+         static['menu_'+name] 为该菜单显示的内容（包括背景和菜单字）
+
+    @para name : 表示这个菜单的名字，将会加载与名字相关的内容。应该为
+                 英文
+    '''
     info_format = "%s区 [%s]"
     
     background = static['menu'].safe_substitute(
@@ -97,14 +135,18 @@ class MenuFrame(Frame):
 
 @mark('unf')
 class UnfinishFrame(Frame):
-
-    def initialize(self):
+    '''
+    未实现的frame。
+    '''
+    def initialize(self,*kwargs):
         self.write("This part isn't finish.")
         self.close()
 
 @mark('bye')
 class ByeFrame(Frame):
-
+    '''
+    正常关闭的frame。
+    '''
     def initialize(self):
         self.write("Bye!.\r\n")
         self.close()
