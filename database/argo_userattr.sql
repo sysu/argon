e835535cc48fd8c823e44efd26c2581190ffa738
wizardcypress <bolin.huang@gmail.com>
2012-06-05 00:59:36 +0800

    modify sql

--- database/argo_userattr.sql
+++ database/argo_userattr.sql
@@ -1,7 +1,22 @@
+/*
+    For example:
+
+    LTS's friends: gcc Cypress
+    a = [['gcc', 'Big cc'], ['Cypress', 'berlin']] //'Big cc' and 'berlin' is remark
+    b = cPickle.dumps(a, 2)
+    friends  = b
+    
+    Others are similar.
+*/
+
 DROP TABLE IF EXISTS `argo_userattr`;
 CREATE TABLE IF NOT EXISTS `argo_userattr` (
     `userid` varchar(20) NOT NULL,
-    `attr` blob,
+    `friends` blob,
+    `rejects` blob,
+    `favorite` blob,
+   
+    /* more */
 
      PRIMARY KEY (`userid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=UTF8;
---
+++ database/template/argo_mailhead.sql
@@ -0,0 +1,21 @@
+/*
+    tableid = uid / 100
+*/
+DROP TABLE IF EXISTS `argo_mailhead_${tableid}`;
+CREATE TABLE IF NOT EXISTS `argo_mailhead_${tableid}` (
+    `mid` int(11) unsigned NOT NULL auto_increment,
+    `fromuserid` varchar(16) NOT NULL,
+    `touserid` varchar(16) NOT NULL,
+    `attachidx` varchar(128),
+    
+    `sendtime` int(11) unsigned NOT NULL default 0,
+    `fromaddr` varchar(64),
+    
+    `readmark` int(11) unsigned NOT NULL default 0,
+    `content` text,
+    `quote` text,
+    `signature` text,
+
+    PRIMARY KEY (`mid`)
+) ENGINE=MyISAM DEFAULT CHARSET=UTF8;
+
