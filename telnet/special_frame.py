b93277d8848c8e6169b4ecdd5d6e59774dfea958
Norman Mo <LTaoist6@gmail.com>
2012-09-02 17:54:05 +0800

    modified bad ending hint

--- telnet/special_frame.py
+++ telnet/special_frame.py
@@ -16,7 +16,7 @@ class BadEndingFrame(BaseAuthedFrame):
             manager.auth.safe_logout(self.userid,self.seid)
         except AttributeError:
             pass            
-        self.write(ac.clear + u'崩溃啦~ T.T 麻烦请报告管理员~\r\n'+ac.reset)
+        self.write(ac.clear + u'崩溃啦~ T.T 麻烦到主菜单使用Bug Report报告~\r\n'+ac.reset)
         self.pause()
         self.close()
 
